ec546d6e03cda398f2f4a2063e79a8aa21bfb9fc
albert <albert.rosell@gmail.com>
2015-04-29 10:03:22 +0200

    simplify scroll

--- src/uiSelectScrollDirective.js
+++ src/uiSelectScrollDirective.js
@@ -1,26 +1,13 @@
-uis.directive('uiSelectScroll',
-  ['uiSelectConfig', 'uiSelectMinErr',
-  function(uiSelectConfig, uiSelectMinErr) {
-
+uis.directive('uiSelectScroll', [function() {
   return {
     restrict: 'EA',
     require: '^uiSelect',
     replace: true,
     transclude: true,
-    templateUrl: function(tElement) {
-      // Gets theme attribute from parent (ui-select)
-      var theme = tElement.parent().attr('theme') || uiSelectConfig.theme;
-      return theme + '/choices.tpl.html';
-    },
-
-    compile: function(tElement, tAttrs) {
-      if (!tAttrs.onScroll) {
-        throw uiSelectMinErr('onScroll', "Expected 'onScroll' expression.");
-      }
-
+    compile: function() {
       return function link(scope, element, attrs, $select) {
         element.bind('scroll', function () {
-          if (element[0].scrollTop + element[0].offsetHeight >= element[0].scrollHeight) {
+          if (attrs.onScroll && element[0].scrollTop + element[0].offsetHeight >= element[0].scrollHeight) {
             $select.onScroll(attrs.onScroll);
           }
         });
